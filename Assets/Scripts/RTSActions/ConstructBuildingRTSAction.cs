cee86405661ef6ab505c05c4135927a04508c627
Joris Laptop <j_d_derks@hotmail.com>
2023-12-07 16:31:49 +0100

    Building construction progress

---
+++ Assets/Scripts/RTSActions/ConstructBuildingRTSAction.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstructBuildingRTSAction : RtsAction
+{
+    private GameObject buildingToConstruct;
+
+    public override void Activate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override ISelectable GetOrigin()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override PanelInfoScriptableObject GetPanelInfo()
+    {
+        throw new System.NotImplementedException();
+    }
+}
