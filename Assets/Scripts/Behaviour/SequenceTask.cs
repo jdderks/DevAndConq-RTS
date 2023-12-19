39c2b0e3ba91b0c3ad6e6df4f3b941a2658d5216
Joris Laptop <j_d_derks@hotmail.com>
2023-12-19 15:21:43 +0100

    behaviour tree sequencetask progress

---
+++ Assets/Scripts/Behaviour/SequenceTask.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceTask : UnitTask
+{
+    int currentTaskIndex = -1;
+    private List<UnitTask> subTasks = new List<UnitTask>();
+
+    public void AddTask(UnitTask task)
+    {
+        subTasks.Add(task);
+    }
+
+    public override void OnBegin()
+    {
+        currentTaskIndex = -1;
+        StartNextTask();
+    }
+
+    public override void OnCancel()
+    {
+        if (currentTaskIndex >= 0 && currentTaskIndex < subTasks.Count)
+        {
+            subTasks[currentTaskIndex].OnCancel();
+            // Remove subscription from last event if any
+            if (currentTaskIndex > 0)
+            {
+                subTasks[currentTaskIndex - 1].Completed -= OnSubTaskCompleted;
+            }
+        }
+    }
+
+    public override void OnComplete()
+    {
+        // All tasks completed
+    }
+
+    private void StartNextTask()
+    {
+        currentTaskIndex++;
+
+        if (currentTaskIndex < subTasks.Count)
+        {
+            subTasks[currentTaskIndex].OnBegin();
+            subTasks[currentTaskIndex].Completed += OnSubTaskCompleted;
+        }
+        else
+        {
+            OnComplete();
+        }
+    }
+
+    private void OnSubTaskCompleted()
+    {
+        subTasks[currentTaskIndex].Completed -= OnSubTaskCompleted;
+        StartNextTask();
+    }
+}
